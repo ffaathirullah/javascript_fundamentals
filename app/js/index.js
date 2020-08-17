b7f3565eff6e89720fea62bea77dc495cd392174
Fachrul <ffaathirullah1@gmail.com>
2020-08-18 04:38:03 +0700

    Array

--- app/js/index.js
+++ app/js/index.js
@@ -4,33 +4,17 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title')
+logTitle('Array')
 /* coding examples */
 
-var name = "Fachrul Faathirullah";
-var age = 21.5;
-var hasDriverLicence = true;
-var empty = undefined;
+var names = ["Alex", "Maria", "Sam"];
+log(names);
+log(names.length);
 
-var person = {
-    name : "fachrul faathirullah",
-    age : 21.5,
-    hasDriverLicence : true,
-    empty : undefined,
-    dateOfbirth : "01/03/2918",
-    address: {
-        Negara : "Indonesia",
-        Kota : "Bandung",
-        postCode: "4025"
-    }
-};
+for (var i of names){
+    log(i);
+}
 
-log(JSON.stringify(person));
-
-log(person.name);
-log(person.age);
-log(person.hasDriverLicence);
-log(person.empty);
-log(JSON.stringify(person.address));
-
-log(Object.values(person));
+names.forEach(function(i, index, {}){
+    log(index + "-" + i);
+});
