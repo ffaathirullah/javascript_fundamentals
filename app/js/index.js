bfcf02ba1b593ffb67df1800fc74552c75ac31d1
Fachrul <ffaathirullah1@gmail.com>
2020-08-18 05:53:14 +0700

    function

--- app/js/index.js
+++ app/js/index.js
@@ -4,17 +4,15 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Array')
+logTitle('Funtion')
 /* coding examples */
 
-var additon = 2 + 2;
-var subtraction = 3-90;
-var division = 10/5;
-var kali = 20*30;
-var modulus = 5%2;
+var addition = (2 +2) + 2;
 
-log(additon);
-log(subtraction);
-log(division);
-log(kali);
-log(modulus);
+log(addition);
+
+function addNumbers(){
+    log("add");
+}
+
+addNumbers();
