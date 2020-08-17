dc30b22f99eb4a6458e899a3f47e4a5b6fbf156a
Fachrul <ffaathirullah1@gmail.com>
2020-08-18 04:09:45 +0700

    Object

--- app/js/index.js
+++ app/js/index.js
@@ -6,3 +6,31 @@ import { log, logTitle } from 'logger';
 /* your imports */
 logTitle('Title')
 /* coding examples */
+
+var name = "Fachrul Faathirullah";
+var age = 21.5;
+var hasDriverLicence = true;
+var empty = undefined;
+
+var person = {
+    name : "fachrul faathirullah",
+    age : 21.5,
+    hasDriverLicence : true,
+    empty : undefined,
+    dateOfbirth : "01/03/2918",
+    address: {
+        Negara : "Indonesia",
+        Kota : "Bandung",
+        postCode: "4025"
+    }
+};
+
+log(JSON.stringify(person));
+
+log(person.name);
+log(person.age);
+log(person.hasDriverLicence);
+log(person.empty);
+log(JSON.stringify(person.address));
+
+log(Object.values(person));
