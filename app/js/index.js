4a3902ca6d802e3672de0843d6fc3c7c3c298434
Fachrul <ffaathirullah1@gmail.com>
2020-08-18 10:57:03 +0700

    map.filter.reduce1

--- app/js/index.js
+++ app/js/index.js
@@ -3,8 +3,26 @@ require('styles/main.scss');
 /* js */
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
+import { map } from 'bluebird';
 /* your imports */
-logTitle('Map || filter reduce')
+logTitle('Map || filter || reduce')
 /* coding examples */
 
 
+var mapa = [1, 2, 3, 4, 5].map(function(n){
+    return n * 2;
+});
+
+log(mapa);
+
+var filter1 = [1, 2, 3, 4, 5, 10 ,20, 100].filter(function(n){
+    return n % 2 == 0;
+});
+
+log(reduce1)
+
+var reduce1 = [1, 2, 3, 4, 5].reduce(function(accumulator, current){
+    return accumulator + current;
+});
+
+log(reduce1)
