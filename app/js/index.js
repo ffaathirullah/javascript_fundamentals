2e43f7d149a81320e9e4fd9d6dac57a822016ec6
Fachrul <ffaathirullah1@gmail.com>
2020-08-18 09:24:37 +0700

    branch and continue

--- app/js/index.js
+++ app/js/index.js
@@ -4,31 +4,28 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Funtion')
+logTitle('Breaks and Continue')
 /* coding examples */
 
+
+log("Break..... :");
 var number = 0;
 while(number < 5){
     log(number);
-    number +=1
+    if (number == 2){
+        break
+    }
+    number++;
 }
 
-var i = 0;
-log("selanjutnya");
-do{
-    i++;
-    log(i);
-}while(i < 5);
+log("Continue..... :");
+var angka = 0;
+while(angka < 5){
+    angka++;
 
-log("selanjutnya");
-var person = [
-    {name : "Fachrul" , age : 21},
-    {name : "Alex", age : 20}
-];
-
-
-for (var i=0; i<=person.length; i++){
-    log(person[i].name);
-    log(person[i].age);
-    log("===========================")
+    if(angka < 5){
+        continue;
+    }
+    log(angka);
 }
+
